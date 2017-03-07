056360b6f6dad90a02e61cf9b5c7d1ff5a49c821
Nicolas Stefani <nicolas471@gmail.com>
2017-03-06 21:45:32 -0300

    include oH_my_zsh intallation

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 
 # create a symbolic link for dotfiles configuration
 ln .dotfilesrc ~/.dotfilesrc
+
+# oh_my_zsh installation
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # dotfiles packages is need
 dotfiles --sync --force
